91e0480205bdaec3ed7fbe9f0986e0266885500c
Digital House <digitalhouse@dh-a3-03.digitalhouse.local>
2016-07-21 20:56:24 -0300

    agregamos imagen en carrito

--- app/index.js
+++ app/index.js
@@ -39,9 +39,6 @@ class SimpleApp extends Component {
     });
   }
 
-
-
-
   onAgregar(id, cantidad) {
     const articulo = this.state.stock.find((producto) => producto.id === id);
     articulo.cantidad = cantidad;
@@ -97,6 +94,7 @@ class ListaProductosEnCarrito extends Component {
           nombre={producto.nombre}
           cantidad={producto.cantidad}
           precio={producto.precio}
+          img={producto.img}
           onOperar={this.props.onRemover}
         />
       );
@@ -195,6 +193,9 @@ class ProductoCarrito extends Component {
   render() {
     return (
       <div className={styles.articulo}>
+      <div className={styles.thum}>
+          <img src={this.props.img}></img>
+      </div>
         <h3>{ this.props.nombre }</h3>
         <h3>Cantidad Comprada : {this.props.cantidad} </h3>
         <h3>Precio Producto: {this.props.precio} </h3>
