6a6b7175c90b259332a529551d0297e660728092
Digital House <digitalhouse@dh-a3-03.digitalhouse.local>
2016-07-21 20:52:31 -0300

    sumamos imagen

--- app/index.js
+++ app/index.js
@@ -22,14 +22,15 @@ class SimpleApp extends Component {
 
     moltin.Authenticate(() => {
       moltin.Product.Search({status: 1}, (products)=> {
-
+        console.log(products[0]);
         const stock = products.map((producto)=>{
           return {
             //description : producto.description,
             id : producto.id,
             precio : producto.price.data.raw.with_tax,
             cantidad : producto.stock_level,
-            nombre : producto.title
+            nombre : producto.title,
+            img : producto.images[0].url.http
           }
         });
 
@@ -37,6 +38,8 @@ class SimpleApp extends Component {
        });
     });
   }
+
+
 
 
   onAgregar(id, cantidad) {
@@ -124,6 +127,7 @@ class ListaProductosOfrecidos extends Component {
           precio={producto.precio}
           nombre={producto.nombre}
           onOperar={this.props.onAgregar}
+          img ={producto.img}
         />
       );
     });
@@ -153,6 +157,9 @@ class ProductoOfrecido extends Component {
   render() {
     return (
       <div className={styles.articulo}>
+      <div className={styles.thum}>
+          <img src={this.props.img}></img>
+      </div>
         <h3>{ this.props.nombre }</h3>
         <h3>{this.props.precio} </h3>
         <input type="number" ref={(c)=>this._cantidad=c}></input>
